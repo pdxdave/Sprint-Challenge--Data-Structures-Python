fbfe94a1e05234aa7eadee37ec10e9d22575b53f
pdxdave <webdevdave@hotmail.com>
2019-09-20 08:20:00 -0700

    working on names. loop through names list

--- names/names.py
+++ names/names.py
@@ -8,18 +8,27 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+lists_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+lists_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# This was the original solution provided with the code
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+first_name_list = NameChecker(lists_1[0])
+for i in range(1, len(lists_1)):
+    first_name_list.insert(lists_1[i])
+
+for list_2 in lists_2:
+    if first_name_list.contains(list_2):
+        duplicates.append(list_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
